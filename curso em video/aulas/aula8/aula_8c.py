ea6ed01ad3b30d26e147d6e5eabb89d5b92fe388
Gustavo Leandro <103152262+gustavolcosta02@users.noreply.github.com>
2023-04-11 10:22:13 -0300

    Update aula_8c.py

--- curso em video/aulas/aula8/aula_8c.py
+++ curso em video/aulas/aula8/aula_8c.py
@@ -1,3 +1,3 @@
-import emoji
-
-print( emoji.emojize('Vai tomar no cu xina seu macaco :monkey: :banana:') )
+#import #emoji
+#uma bibilhotera que não uso mais
+#print( emoji.emojize('Vai tomar no cu xina seu macaco :monkey: :banana:') )
